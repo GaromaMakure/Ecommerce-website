e93cb0d393ce2399e6f0f0d45717c344223b8784
GaromaMakure <garomamak11@gmail.com>
2025-01-16 10:59:11 -0800

    updating navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-
+import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Electronics",
+    link: "/#",
+  },
+];
 const Navbar = () => {
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -16,7 +44,7 @@ const Navbar = () => {
           </div>
           {/* search bar and order button */}
 
-          <div>
+          <div className="flex justify-between items-center gap-4">
             <div className="relative group hidden sm:block">
               <input
                 type="text"
@@ -35,12 +63,27 @@ const Navbar = () => {
             className="bg-gradient-to-r from-primary to-secondary transition-all duration-200
           text-white py-1 px-4 rounded-full flex items-center gap-3 group"
           >
-            <span className="group-hover:block hidden">order</span>
+            <span className="group-hover:block hidden transition-all duration-200">
+              order
+            </span>
+            <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
           </button>
+          {/* Darkmode switch */}
+          <div>
+            <DarkMode />
+          </div>
         </div>
       </div>
       {/* lower Navbar */}
-      <div></div>
+      <div>
+        <ul>
+          {menu.map((data) => (
+            <li>
+              <a href={data.link}>{data.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
