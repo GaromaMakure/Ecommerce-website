2dcc7755f1f8e5c2e6888065b5c5e647dc58248f
GaromaMakure <garomamak11@gmail.com>
2025-01-26 03:41:50 -0800

    updating the src

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -93,9 +93,9 @@ const Products = () => {
           </div>
           {/* view all button */}
           <div className="flex justify-center">
-            {/* <button className="text-center mt-10 cursor-pointer bg-primary text-white py-1 px-5 rounded-md">
+            <button className="text-center mt-10 cursor-pointer bg-primary text-white py-1 px-5 rounded-md">
               View All Button
-            </button> */}
+            </button>
           </div>
         </div>
       </div>
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="py-10 mb-10">
+      <div className="container">
+        {/* header section part */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+          <p data-aos="fade-up" className="text-sm text-primary">
+            what are customers saying about us
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Testimonials
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            many customers are using our products because our products
+            originality and the care we give for our customers is so amazing so
+            many of our customers are loving giving as credit
+          </p>
+        </div>
+        {/* Testimonial cards */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
